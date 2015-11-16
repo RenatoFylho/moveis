71fed6e5a5eb512b55601554fe259a433f1cd7d4
Jose Ferreira <renatofylho@gmail.com>
2015-11-16 10:16:34 -0300

    Alterando textos do banner

--- js/principal.js
+++ js/principal.js
@@ -1,6 +1,7 @@
 var banners =
 	["Os melhores do Brasil!", "Qualidade e preço baixo!"];
 var bannerAtual = 0;
+var banners = ["Do lixo ao luxo!","Reaproveitar é aproveitar!"];
 function trocaBanner() {
 	bannerAtual = (bannerAtual + 1) % 2;
 	document.querySelector(’h2#mensagem’).textContent =
